547f01bb4d87ba139e8084bc7706d58a7e24d79f
probability12 <congduydanh@gmail.com>
2025-11-25 14:33:07 +0700

    Update BossUtil.java

--- src/main/java/net/kayn/fallen_gems_affixes/util/BossUtil.java
+++ src/main/java/net/kayn/fallen_gems_affixes/util/BossUtil.java
@@ -1,22 +1,25 @@
 package net.kayn.fallen_gems_affixes.util;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.item.Item;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
 
 public class BossUtil {
 
-    public static final TagKey<EntityType<?>> BOSS_TAG = TagKey.create(
-            Registries.ENTITY_TYPE,
-            new ResourceLocation("forge", "bosses")
-    );
+    private static final TagKey<EntityType<?>> FORGE_BOSSES =
+            TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation("forge:bosses"));
 
-    public static Item getApotheosisGem() {
-        return ForgeRegistries.ITEMS.getHolder(new ResourceLocation("apotheosis", "gem"))
-                .map(h -> h.value())
-                .orElse(null);
+    public static boolean isBoss(LivingEntity entity, TagKey<EntityType<?>> extraBossTag) {
+        return entity.getType().is(FORGE_BOSSES)
+                || entity.getType().is(extraBossTag)
+                || isApotheosisBoss(entity);
+    }
+
+    public static boolean isApotheosisBoss(LivingEntity entity) {
+        CompoundTag tag = entity.getPersistentData();
+        return tag.getBoolean("apoth.boss");
     }
 }
